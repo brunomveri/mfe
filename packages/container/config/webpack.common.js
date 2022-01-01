8336c2f5ff0a98c4e53528ab7c30b00bdd98fce0
Bruno Verissimo <bruno.verissimo@justeattakeaway.com>
2021-12-31 17:03:19 -0700

    created container deploy script

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,17 +1,19 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin"); // This is what is going to take some HTML file inside our project and inject a couple
+// // of different script tags inside of it.
+
 module.exports = {
   module: {
     rules: [
-      // The goal of a loader is to tell webpack to process some different files as we start to import them.
       {
         test: /\.m?js$/, // this says that whenever we import in a file that ends with mjs or js, we want it to be processed by Babel.
         exclude: /node_modules/, // do not try to run Babel on any file of our node_modules directory.
         use: {
-          loader: "babel-loader",
+          loader: "babel-loader", // The goal of a loader is to tell webpack to process some different files as we start to import them.
           options: {
             presets: [
               "@babel/preset-react" /* processes JSX */,
-              "@babel/preset-env" /* converts ES6+ into ES5 */,
-            ],
+              "@babel/preset-env",
+            ] /* converts ES6+ into ES5 */,
             plugins: [
               "@babel/plugin-transform-runtime",
             ] /* adds in a little additional code to enable different features for our project inside the browser such as async/await syntax */,
@@ -20,4 +22,9 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
