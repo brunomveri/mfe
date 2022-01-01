9e47db43781d78b7db9cb73bd6c2862ed32c065e
Bruno Verissimo <bruno.verissimo@justeattakeaway.com>
2021-12-31 18:54:07 -0700

    fixed container common webpack settings

--- packages/container/config/webpack.common.js
+++ packages/container/config/webpack.common.js
@@ -1,30 +1,55 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin"); // This is what is going to take some HTML file inside our project and inject a couple
-// // of different script tags inside of it.
+// const HtmlWebpackPlugin = require("html-webpack-plugin"); // This is what is going to take some HTML file inside our project and inject a couple
+// // // of different script tags inside of it.
+
+// module.exports = {
+//   module: {
+//     rules: [
+//       {
+//         test: /\.m?js$/, // this says that whenever we import in a file that ends with mjs or js, we want it to be processed by Babel.
+//         exclude: /node_modules/, // do not try to run Babel on any file of our node_modules directory.
+//         use: {
+//           loader: "babel-loader", // The goal of a loader is to tell webpack to process some different files as we start to import them.
+//           options: {
+//             presets: [
+//               "@babel/preset-react" /* processes JSX */,
+//               "@babel/preset-env",
+//             ] /* converts ES6+ into ES5 */,
+//             plugins: [
+//               "@babel/plugin-transform-runtime",
+//             ] /* adds in a little additional code to enable different features for our project inside the browser such as async/await syntax */,
+//           },
+//         },
+//       },
+//     ],
+//     plugins: [
+//       new HtmlWebpackPlugin({
+//         template: "./public/index.html",
+//       }),
+//     ],
+//   },
+// };
+
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   module: {
     rules: [
       {
-        test: /\.m?js$/, // this says that whenever we import in a file that ends with mjs or js, we want it to be processed by Babel.
-        exclude: /node_modules/, // do not try to run Babel on any file of our node_modules directory.
+        test: /\.m?js$/,
+        exclude: /node_modules/,
         use: {
-          loader: "babel-loader", // The goal of a loader is to tell webpack to process some different files as we start to import them.
+          loader: "babel-loader",
           options: {
-            presets: [
-              "@babel/preset-react" /* processes JSX */,
-              "@babel/preset-env",
-            ] /* converts ES6+ into ES5 */,
-            plugins: [
-              "@babel/plugin-transform-runtime",
-            ] /* adds in a little additional code to enable different features for our project inside the browser such as async/await syntax */,
+            presets: ["@babel/preset-react", "@babel/preset-env"],
+            plugins: ["@babel/plugin-transform-runtime"],
           },
         },
       },
     ],
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: "./public/index.html",
-      }),
-    ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
 };
